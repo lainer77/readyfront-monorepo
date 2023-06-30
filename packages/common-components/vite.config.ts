6320a40c8ad2e4fb1a2389679a6ba342deab297c
sunaliner <dh.kim@hanwha-nxef.com>
2023-06-30 15:16:25 +0900

    fix: eslint error

--- packages/common-components/vite.config.ts
+++ packages/common-components/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
     build: {
         lib: {
             entry: resolve(__dirname, 'lib/main.ts'),
-            name: 'MyLib',
             // 적절한 확장자가 추가됩니다.
             fileName: 'my-lib',
+            name: 'MyLib',
         },
     },
+    plugins: [react()],
 });
--- products/my-app/src/App.tsx
+++ products/my-app/src/App.tsx
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 import { testLog } from '@common/utils';
 
+import './App.css';
+import logo from './logo.svg';
+
 function App() {
-    testLog({ name: 'test', age: 17, gender: 'male', memo: 'tetstetsets' });
+    testLog({ age: 17, gender: 'male', memo: 'tetstetsets', name: 'test' });
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img alt="logo" className="App-logo" src={logo} />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
-                    target="_blank"
                     rel="noopener noreferrer"
+                    target="_blank"
                 >
                     Learn React
                 </a>
