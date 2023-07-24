8092787afbd10e5c696d58743b4f2d26a9ab0858
sunaliner <dh.kim@hanwha-nxef.com>
2023-07-24 22:47:42 +0900

    refactor: styles 보간 고려

--- projects/vite-ssr-project/components/RenderMark.tsx
+++ projects/vite-ssr-project/components/RenderMark.tsx
@@ -1,15 +1,12 @@
 import { styled } from 'styled-components';
 export type tRenderMarkType = 'CSR' | 'ISR' | 'SSG' | 'SSR';
-const MarkStyled = styled.div<{ type: tRenderMarkType }>`
+const MarkStyled = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     width: 4rem;
     height: 4rem;
     border-radius: 50%;
-    background-color: ${(props) =>
-        ({ CSR: 'red', ISR: 'yellow', SSG: 'green', SSR: 'blue' }[props.type])};
-    color: ${(props) => ({ CSR: 'white', ISR: 'blue', SSG: 'yellow', SSR: 'white' }[props.type])};
     position: fixed;
     bottom: 1rem;
     right: 1rem;
@@ -17,5 +14,14 @@ const MarkStyled = styled.div<{ type: tRenderMarkType }>`
     opacity: 0.4;
 `;
 export default function RenderMark({ type }: { type: tRenderMarkType }) {
-    return <MarkStyled type={type}>{type}</MarkStyled>;
+    return (
+        <MarkStyled
+            style={{
+                backgroundColor: { CSR: 'red', ISR: 'yellow', SSG: 'green', SSR: 'blue' }[type],
+                color: { CSR: 'white', ISR: 'blue', SSG: 'yellow', SSR: 'white' }[type],
+            }}
+        >
+            {type}
+        </MarkStyled>
+    );
 }
