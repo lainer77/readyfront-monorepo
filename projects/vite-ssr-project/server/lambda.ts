a9590ee1b3af16146890d8de5f61e340e9277080
sunaliner <dh.kim@hanwha-nxef.com>
2023-11-09 15:00:32 +0900

    fix: Scheduled Event return

--- projects/vite-ssr-project/server/lambda.ts
+++ projects/vite-ssr-project/server/lambda.ts
@@ -2,4 +2,8 @@ import serverless from 'serverless-http';
 
 import { app } from './app';
 
-export const handler = serverless(app);
+export const handler = (event: { 'detail-type': string }, context: object) => {
+    if (event['detail-type'] === 'Scheduled Event')
+        return { headers: { 'Content-Type': 'application/json' }, statusCode: 200 };
+    return serverless(app)(event, context);
+};
